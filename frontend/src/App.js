42cd9de4bd740d6a181d45ca614b1c12546e41a1
hades42 <nguyenvannguyen.oc@gmail.com>
2021-08-21 14:33:35 +1000

    Adding HomeScreen to App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,17 @@
-import "./App.css";
-
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Container } from "react-bootstrap";
+import HomeScreen from "./screens/HomeScreen";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+      <main>
+        <HomeScreen></HomeScreen>
+      </main>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,13 @@
+import React from "react";
+import IntroSection from "../components/IntroSection";
+import MainSection from "../components/MainSection";
+const HomeScreen = () => {
+  return (
+    <div>
+      <IntroSection></IntroSection>
+      <MainSection></MainSection>
+    </div>
+  );
+};
+
+export default HomeScreen;
