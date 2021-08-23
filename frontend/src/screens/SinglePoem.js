8b1d03434fe6fdf88cf9cff66ca8300de9e17ad9
hades42 <nguyenvannguyen.oc@gmail.com>
2021-08-23 14:16:26 +1000

    Using redux state for handle data for singlePoem

--- frontend/src/screens/SinglePoem.js
+++ frontend/src/screens/SinglePoem.js
@@ -1,11 +1,18 @@
 import React from "react";
-import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
 import classes from "./SinglePoem.module.css";
 import ReactMarkdown from "react-markdown";
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import remarkGfm from "remark-gfm";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { nord } from "react-syntax-highlighter/dist/esm/styles/prism";
+import {
+  getSinglePoem,
+  upVoteSinglePoem,
+  downVoteSinglePoem,
+} from "../actions/poemActions";
+import Loader from "../components/Loader";
+import Message from "../components/Message";
 
 const SinglePoem = ({ match }) => {
   const components = {
@@ -26,82 +33,78 @@ const SinglePoem = ({ match }) => {
       );
     },
   };
-  const poemId = match.params.id;
-  const [singlePoem, setSinglePoem] = useState({});
-  const [currentVote, setCurrentVote] = useState(0);
-  useEffect(() => {
-    const fetchData = async () => {
-      const config = {
-        headers: {
-          bob: "Bobalooba",
-        },
-      };
-      const { data } = await axios.get(`/api/poems/${poemId}`, config);
-      console.log(data);
-      setSinglePoem(data);
-      setCurrentVote(data.votes);
-    };
-    fetchData();
-  }, [poemId]);
 
-  const upVote = async () => {
-    const config = {
-      headers: {
-        bob: "Bobalooba",
-      },
-    };
-    const { data } = await axios.post(
-      `https://shielded-spire-87442.herokuapp.com/api/poems/${poemId}`,
-      {},
-      config
-    );
-    setCurrentVote(data.votes);
-  };
+  const poemId = match.params.id;
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getSinglePoem(poemId));
+  }, [poemId, dispatch]);
+
+  const singlePoem = useSelector((state) => state.singlePoem);
+  const { loading, error, poem } = singlePoem;
+
+  const singleVotes = useSelector((state) => state.singleVotes);
+  const { votes, error: errorVote } = singleVotes;
+
   const upVoteHandler = () => {
-    upVote();
+    dispatch(upVoteSinglePoem(poemId));
   };
+  const downVoteHandler = () => {
+    dispatch(downVoteSinglePoem(poemId));
+  };
+  if (errorVote) {
+    alert(errorVote);
+  }
 
   return (
     <>
-      <div className={classes.space}></div>
-      <div className={classes.wrapper}>
-        <div className={classes.container}>
-          <div className={classes.vote}>
-            <div onClick={upVoteHandler} className={classes.upVote}>
-              <i className="fas fa-chevron-up"></i>
-            </div>
-            <p>{currentVote}</p>
-            <div className={classes.downVote}>
-              <i className="fas fa-chevron-down"></i>
-            </div>
-          </div>
+      {loading ? (
+        <Loader></Loader>
+      ) : error ? (
+        <Message variant="danger">{error}</Message>
+      ) : (
+        <>
+          <div className={classes.space}></div>
+          <div className={classes.wrapper}>
+            <div className={classes.container}>
+              <div className={classes.vote}>
+                <div onClick={upVoteHandler} className={classes.upVote}>
+                  <i className="fas fa-chevron-up"></i>
+                </div>
+                <p>{votes}</p>
+                <div onClick={downVoteHandler} className={classes.downVote}>
+                  <i className="fas fa-chevron-down"></i>
+                </div>
+              </div>
 
-          <div className={classes.heading}>
-            <h3 to={`/poem/${singlePoem.id}`} className={classes.title}>
-              {singlePoem.title}
-            </h3>
-            <div className={classes.author}>
-              <p>By {singlePoem.author}</p>
+              <div className={classes.heading}>
+                <h3 className={classes.title}>{poem.title}</h3>
+                <div className={classes.author}>
+                  <p>By {poem.author}</p>
+                </div>
+                {poem.title ? (
+                  <div className={classes.coverLetter}>{poem.title[0]}</div>
+                ) : (
+                  ""
+                )}
+              </div>
+              <div className={classes.main}>
+                <ReactMarkdown
+                  components={components}
+                  remarkPlugins={[remarkGfm]}
+                  children={poem.text}
+                  className={classes.content}
+                ></ReactMarkdown>
+              </div>
+              <div className={classes.footer}>
+                <button onClick={upVoteHandler} className={classes.voteBtn}>
+                  Vote
+                </button>
+              </div>
             </div>
-            {singlePoem.title ? (
-              <div className={classes.coverLetter}>{singlePoem.title[0]}</div>
-            ) : (
-              ""
-            )}
           </div>
-          <div className={classes.main}>
-            <ReactMarkdown
-              components={components}
-              remarkPlugins={[remarkGfm]}
-              children={singlePoem.text}
-              className={classes.content}
-            ></ReactMarkdown>
-          </div>
-          <div className={classes.footer}>
-            <button className={classes.voteBtn}>Vote</button>
-          </div>
-        </div>
-      </div>
+        </>
+      )}
     </>
   );
 };
