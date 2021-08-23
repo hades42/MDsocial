7a579f65846de09707099e0bca0f1f1180ac8da5
hades42 <nguyenvannguyen.oc@gmail.com>
2021-08-23 14:14:10 +1000

    Using redux state for creating new poem

--- frontend/src/screens/CreatingNewPoem.js
+++ frontend/src/screens/CreatingNewPoem.js
@@ -1,28 +1,32 @@
-import React, { useState } from "react";
-import axios from "axios";
+import React, { useEffect, useState } from "react";
 import classes from "./CreatingNewPoem.module.css";
 import MarkdownEditor from "../components/MarkdownEditor";
+import { useDispatch, useSelector } from "react-redux";
+import { addNewPoem } from "../actions/poemActions";
+import Loader from "../components/Loader";
+import Message from "../components/Message";
 const CreatingNewPoem = () => {
   const [title, setTitle] = useState("");
   const [author, setAuthor] = useState("");
   const [text, setText] = useState("");
-
   const onChange = (value) => {
     setText(value);
   };
-  const submitData = async (formData) => {
-    const config = {
-      headers: {
-        bob: "Bobalooba",
-      },
-    };
-    const { data } = await axios.post(
-      `https://shielded-spire-87442.herokuapp.com/api/poems`,
-      formData,
-      config
-    );
-    console.log(data);
-  };
+  const dispatch = useDispatch();
+
+  const addPoem = useSelector((state) => state.addPoem);
+  const { loading, error, data } = addPoem;
+
+  useEffect(() => {
+    if (data) {
+      if (!error) {
+        setTitle("");
+        setAuthor("");
+        setText("");
+      }
+    }
+  }, [data, error]);
+
   const submitHandler = (e) => {
     e.preventDefault();
     const formData = {
@@ -30,38 +34,40 @@ const CreatingNewPoem = () => {
       author,
       text,
     };
-    submitData(formData);
-    setTitle("");
-    setAuthor("");
-    setText("");
+    dispatch(addNewPoem(formData));
   };
   return (
-    <form className={classes.form} onSubmit={submitHandler}>
-      <div className={classes.box}>
-        <div className={classes.title}>
-          <label htmlFor="title">Title of poem</label>
-          <input
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-            type="text"
-            id="title"
-          ></input>
+    <>
+      {loading && <Loader></Loader>}
+      {error && <Message variant="danger">{error}</Message>}
+      {data && <Message variant="success">{data.message}</Message>}
+      <form className={classes.form} onSubmit={submitHandler}>
+        <div className={classes.box}>
+          <div className={classes.title}>
+            <label htmlFor="title">Title of poem</label>
+            <input
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              type="text"
+              id="title"
+            ></input>
+          </div>
+          <div className={classes.author}>
+            <label htmlFor="author">Your name</label>
+            <input
+              value={author}
+              onChange={(e) => setAuthor(e.target.value)}
+              type="text"
+              id="author"
+            ></input>
+          </div>
+          <button type="submit" className={classes.submit}>
+            Submit
+          </button>
         </div>
-        <div className={classes.author}>
-          <label htmlFor="author">Your name</label>
-          <input
-            value={author}
-            onChange={(e) => setAuthor(e.target.value)}
-            type="text"
-            id="author"
-          ></input>
-        </div>
-        <button type="submit" className={classes.submit}>
-          Submit
-        </button>
-      </div>
-      <MarkdownEditor text={text} onChange={onChange}></MarkdownEditor>
-    </form>
+        <MarkdownEditor text={text} onChange={onChange}></MarkdownEditor>
+      </form>
+    </>
   );
 };
 
