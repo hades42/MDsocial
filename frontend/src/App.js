54b8ff3f22dd6a866b7096c0815d6e5409bd45f3
hades42 <nguyenvannguyen.oc@gmail.com>
2021-08-24 18:26:06 +1000

    Adding login screen to the route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,12 +4,14 @@ import Footer from "./components/Footer";
 import HomeScreen from "./screens/HomeScreen";
 import SinglePoem from "./screens/SinglePoem";
 import CreatingNewPoem from "./screens/CreatingNewPoem";
+import LoginScreen from "./screens/LoginScreen";
 function App() {
   return (
     <Router>
       <div className="App">
         <Header />
         <main>
+          <Route path="/login" component={LoginScreen}></Route>
           <Route path="/createPoem" component={CreatingNewPoem}></Route>
           <Route path="/poems/:id" component={SinglePoem}></Route>
           <Route path="/" component={HomeScreen} exact></Route>
