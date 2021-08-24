d74f56247d5ec8f4748ef76c17e0533f6db64c7b
hades42 <nguyenvannguyen.oc@gmail.com>
2021-08-24 14:50:09 +1000

    Adding middleware for authentication when do auth activities

--- backend/middleWare/authMiddleware.js
+++ backend/middleWare/authMiddleware.js
@@ -1,3 +1,7 @@
+import jwt from "jsonwebtoken";
+import asyncHandler from "express-async-handler";
+import users, { findUserById } from "../data/users.js";
+
 const headerProtect = (req, res, next) => {
   if (req.headers["bob"] === "Bobalooba") {
     next();
@@ -7,4 +11,38 @@ const headerProtect = (req, res, next) => {
   }
 };
 
-export { headerProtect };
+const protect = asyncHandler((req, res, next) => {
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      token = req.headers.authorization.split(" ")[1];
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+      const returnUser = findUserById(decoded.id);
+      if (!returnUser) {
+        res.status(401);
+        throw new Error("Not authorized, please sign in");
+      }
+      req.user = {
+        id: returnUser.id,
+        name: returnUser.name,
+        email: returnUser.email,
+        isAdmin: returnUser.isAdmin,
+      };
+      next();
+    } catch (error) {
+      console.error(error);
+      res.status(401);
+      throw new Error("Not authorized, token failed");
+    }
+  }
+  if (!token) {
+    res.status(401);
+    throw new Error("Not authorized, please sign in");
+  }
+});
+
+export { headerProtect, protect };
